942b70e5fbc550168c6d3b19345053a8b6fef962
Miguel Gastalver Trujillo <mgastalver@gmail.com>
2017-02-21 00:06:11 +0100

    Simplificado servidor

--- server/server.js
+++ server/server.js
@@ -7,12 +7,12 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var debug = require('debug')('conferencia:server');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
+var index = require('./routes/index');
+var users = require('./routes/users');
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -29,7 +29,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('../public'));
+app.use(express.static('../../public'));
 
 app.use('/', index);
 app.use('/users', users);
